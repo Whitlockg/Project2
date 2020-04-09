abd15f3cb1a903cbe7330c7db2641027dff8ad91
Whitlockg <christinw@nevada.unr.edu>
2020-04-09 18:31:06 -0400

    4/9 update

--- static/js/tables.js
+++ static/js/tables.js
@@ -1,15 +1,35 @@
 // Get the corona virus data and start plotting:
-// TO DO - Later replace d3.csv with d3.json
-d3.csv("../../data/coronadata.csv", function(Cases_table) {
-    var parsedCSV = d3.csvParseRows(Cases_table);
+// get table references
+const tbody = d3.select("tbody");
+function country_data(data, colVal) {
+    // console.log(data);    
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+        // console.log(dataRow);
+        // Append a row to the table body
+        const row = tbody.append("tr");
+        // Go through relevant columns and add
+        let cell1 = row.append("td");
+        cell1.text(dataRow.country_name);
+        // if function doesn't work:
+        // if colVal == 'num_cases'
+        let cell2 = row.append("td");
+        cell2.text(dataRow.colVal.sort(d3.descending));
+});
 
-    var container = d3.select("body")
-        .append("table")
-        .selectAll("tr")
-            .Cases_table(parsedCSV).enter()
-            .append("tr")
-        .selectAll("td")
-            .Cases_table(function(d) {return d;}).enter()
-            .append("td")
-            .csv(function(d) {return d;});
-})
+function handleClick(data) {
+    const colVal = d3.select("#countries_table").property("value");
+    country_data(data, colVal);
+}
+
+// TO DO - Later replace d3.csv with d3.json
+d3.csv("../static/data/coronadata.csv").then((data) => {
+
+    // will this work???
+    country_data(data, num_cases);
+
+    // TO DO - change to respond to dropdown selection
+    // event listener part
+    d3.selectAll("#filter-btn").on("click", handleClick(data));
+});
